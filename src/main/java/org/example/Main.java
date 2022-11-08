cc73c68fa76e7e255814c60fd5346d13fee32d9c
daya <dayajuangmahaputra@gmail.com>
2022-11-08 21:17:45 +0900

    initial commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,19 @@
-package org.example;public class Main {
+package org.example;
+
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        boolean ans = hasTeen(1,30,20);
+        System.out.println(ans);
+    }
+
+    public static boolean hasTeen(int num1,int num2,int num3){
+        return (isTeen(num1) || isTeen(num2) || isTeen(num3));
+    }
+
+    public static boolean isTeen(int number){
+        int minRange = 13;
+        int maxRange = 19;
+
+        return (number >= minRange && number <= maxRange);
     }
 }
